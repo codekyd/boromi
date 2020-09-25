f36f3550d6bf8c6d13afc4d53bc9d2159b426abb
codekyd <okpusjamin@gmail.com>
2020-09-25 02:07:02 +0100

    create Homepage view

--- src/Components/views/Home.js
+++ src/Components/views/Home.js
@@ -1,13 +1,53 @@
 import React from "react";
+import { Typography, Box, Container, makeStyles } from "@material-ui/core"
 import Layout from "../Layout/Layout";
+import { SecondaryButton } from "../Buttons/Buttons";
 
-
-
+const styles = makeStyles({
+	root: {
+		minHeight: "100vh",
+		top: "10%",
+		background: "#1a268f",
+		height: "auto"
+	},
+	introSection: {
+		color: "#fff",
+		display: "flex",
+		marginTop: "20%",
+		alignItems: "center",
+		justifyContent: "center"
+	},
+	introTextSmall: {
+		marginBottom: "40px"
+	}
+})
 const Home = () => {
+	const classes = styles()
 	return(
 		<Layout>
+			<section className={classes.root}>
+				<Container maxWidth="md">
+					<Box className={classes.introSection}>
+						<Box>
+							<Typography variant="h2" gutterBottom>
+								Get Access To Instant Fast Loans With Low Interest Rate.
+							</Typography>
+							<Typography variant="h5" gutterBottom className={classes.introTextSmall}>
+								No Colleteral, No Delays.
+							</Typography>
+							<SecondaryButton linkTo="/login" content="Get Started"/>
+						</Box>
+					</Box>
+				</Container>
+			</section>
 
-			<h1>Hi</h1>
+			<section>
+				<Container maxWidth="md">
+				<Typography variant="h3">
+					Why Choose Us ?
+				</Typography>
+				</Container>
+			</section>
 		</Layout>
 	)
 }
