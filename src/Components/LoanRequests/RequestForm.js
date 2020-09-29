c45583571b7c053c664789412b5ed81e08355508
codekyd <okpusjamin@gmail.com>
2020-09-29 17:28:17 +0100

    removed commas

--- src/Components/LoanRequests/RequestForm.js
+++ src/Components/LoanRequests/RequestForm.js
@@ -16,7 +16,7 @@ import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
 import { getAllLoans } from "../../actions/loans"
 import PropTypes from "prop-types";
-import {expectedDuration, formatMoney} from "../../utils/helpers";
+import { formatMoney} from "../../utils/helpers";
 import InputLabel from "@material-ui/core/InputLabel";
 
 
